c5ab97ea22aaf3290a3895452550843bbb0cb806
Matthew Baxter <matthewbaxter0412@hotmail.com>
2025-07-16 13:21:06 -0400

    Turning monrepo to separate repos for all projects

--- rust_kafka/src/air_models/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod air_model;
-pub mod api_model;
-
-pub use air_model::{AirQualityHourly, RawAirQuality};
-pub use api_model::APIFetcher;
--- rust_kafka/src/kafka/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod consumer;
-pub mod producer;
-
-pub use consumer::run_consumer;
-pub use producer::{run_historical_producer, run_recent_producer};
